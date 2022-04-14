1e8576aa924fc17e1aa6cfe3adbf8d10b3b40313
Joao-Gontijo <jvgontijo166@gmail.com>
2022-04-14 14:26:01 -0300

    ordenando por nome e por tempo de aula

--- src/io/github/jvgontijo/TestaListaAulas.java
+++ src/io/github/jvgontijo/TestaListaAulas.java
@@ -1,6 +1,8 @@
 package io.github.jvgontijo;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 import io.github.jvgontijo.model.Aula;
 
@@ -9,11 +11,19 @@ public class TestaListaAulas {
 		
 		Aula a1 = new Aula("Trabalhando com ArrayList", 25);
 		Aula a2 = new Aula("Listas de Objetos", 19);
+		Aula a3 = new Aula("Aprendendo Lógica de Programação", 20);
 		
 		ArrayList<Aula> aulas = new ArrayList<Aula>();
 		aulas.add(a1);
 		aulas.add(a2);
+		aulas.add(a3);
 		
+		//ordenando por nome de acordo com toString
+		Collections.sort(aulas);
+		System.out.println(aulas);
+		
+		//ordenando por tempo
+		aulas.sort(Comparator.comparing(Aula::getTempo));
 		System.out.println(aulas);
 	}
 }
--- src/io/github/jvgontijo/model/Aula.java
+++ src/io/github/jvgontijo/model/Aula.java
@@ -1,25 +1,32 @@
 package io.github.jvgontijo.model;
 
-public class Aula {
-	
+import java.util.Comparator;
+
+public class Aula implements Comparable<Aula> {
+
 	private String titulo;
 	private int tempo;
-	
+
 	public Aula(String titulo, int minutos) {
 		this.titulo = titulo;
 		this.tempo = minutos;
 	}
-	
+
 	public String getTitulo() {
 		return titulo;
 	}
-	
-	public int getMinutos() {
+
+	public int getTempo() {
 		return tempo;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "[Aula: " + this.titulo + ", Tempo: " + this.tempo + " minutos]";
+	}
+
+	@Override
+	public int compareTo(Aula outraAula) {
+		return this.titulo.compareTo(outraAula.getTitulo());
 	}
 }
