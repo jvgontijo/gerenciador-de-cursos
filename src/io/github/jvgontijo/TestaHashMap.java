a31982a63ff8f45dfa96019c1bbae07726e719d0
Joao-Gontijo <jvgontijo166@gmail.com>
2022-04-16 16:11:42 -0300

    testando hashmap para pegar key ou valor

---
+++ src/io/github/jvgontijo/TestaHashMap.java
@@ -0,0 +1,26 @@
+package io.github.jvgontijo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestaHashMap {
+	public static void main(String[] args) {
+		
+		Map<String, Integer> nomesParaIdade = new HashMap<>();
+        nomesParaIdade.put("Paulo", 31);
+        nomesParaIdade.put("Adriano", 25);
+        nomesParaIdade.put("Alberto", 33);
+        nomesParaIdade.put("Guilherme", 26);
+        
+        
+        //iteirando a key
+        for (String string : nomesParaIdade.keySet()) {
+			System.out.println(string);
+		}
+        
+        //iteirando o valor
+        for (Integer idade : nomesParaIdade.values()) {
+        	System.out.println(idade);
+		}
+	}
+}
